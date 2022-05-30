4130124e33251c1ffd790d06a06bb976bcb4dffc
andidietz <alexandradietz3@gmail.com>
2022-05-30 15:22:18 -0500

    update files

--- backend/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/users', function(req, res) {
-    return res.json(["user1", "USER 2"])
-})    
-
-module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 "use strict";
 
-const express =require('express')
-const cors = require('cors')
+const app = require("./app");
 
-const app = express()
+
+const { PORT } = require("./config");
+const cors = require('cors')
 app.use(cors())
 
-app.use("/users", require('./routes/routes'))
+// app.use("/users", require('./routes/routes'))
 
-app.listen(3001, function () {
-  console.log(`Started on http://localhost:3001`);
+app.listen(PORT, function () {
+  console.log(`Started on http://localhost:${PORT}`);
 });
